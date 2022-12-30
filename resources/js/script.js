353aa1100e1a1d4c81b97bff7e432604f898230e
Adikumaw <kumawataditya105@gmail.com>
2022-12-30 21:38:20 +0530

    correction for small screens

--- resources/js/script.js
+++ resources/js/script.js
@@ -86,3 +86,21 @@ $(document).ready(function () {
     }
   );
 });
+
+const swiper = new Swiper('.swiper', {
+autoplay: {
+  delay: 1000,
+  disableOnInteraction: false,
+},
+  loop: true,
+
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
